84ab038bb8334a3602496a3161b8680cb625023a
PavelLikharev <pavel7321@mail.ru>
2019-06-14 00:24:36 +0700

    Исправил замечания Тревиса

--- js/stat.js
+++ js/stat.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
@@ -13,12 +15,12 @@ var FONT = '16px PT Mono';
 
 var columnColor = ['rgba(0, 0, 255, 0.2)', 'rgba(0, 0, 255, 0.4)', 'rgba(0, 0, 255, 0.6)', 'rgba(0, 0, 255, 0.8)', 'rgba(0, 0, 255, 1)'];
 
-var renderCloud = function(ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getMaxElement = function(arr) {
+var getMaxElement = function (arr) {
   var maxElement = arr[0];
 
   for (var i = 0; i < arr.length; i++) {
@@ -30,7 +32,7 @@ var getMaxElement = function(arr) {
   return maxElement;
 };
 
-window.renderStatistics = function(ctx, names, times) {
+window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#ffffff');
 
@@ -46,12 +48,12 @@ window.renderStatistics = function(ctx, names, times) {
     ctx.fillText(Math.round(times[i]), CLOUD_X + GAP_IN + (COLUMN_GAP + COLUMN_WIDTH) * i, (COLUMN_HEIGHT + TEXT_HEIGHT + FONT_GAP + (-COLUMN_HEIGHT * times[i]) / maxTime));
     ctx.fillText(names[i], CLOUD_X + GAP_IN + (COLUMN_GAP + COLUMN_WIDTH) * i, CLOUD_Y + FONT_GAP + GAP + GAP + TEXT_HEIGHT + COLUMN_HEIGHT);
 
-    if (names[i] ===  'Вы') {
+    if (names[i] === 'Вы') {
       ctx.fillStyle = '#ff0000';
     } else {
       ctx.fillStyle = columnColor[i];
-    };
+    }
 
     ctx.fillRect(CLOUD_X + GAP_IN + (COLUMN_GAP + COLUMN_WIDTH) * i, CLOUD_Y + TEXT_HEIGHT + COLUMN_HEIGHT + FONT_GAP, COLUMN_WIDTH, (-COLUMN_HEIGHT * times[i]) / maxTime);
-  };
-}
+  }
+};
